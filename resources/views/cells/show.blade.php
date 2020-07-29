0250dc895ce12c65345cfdd85e0a8ea413a8c5d1
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-29 21:37:20 +0500

    Show cell's folder

---
+++ resources/views/cells/show.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('title', "Шкаф {$cell->title}")
+
+@section('breadcrumbs')
+    <li class="breadcrumb-item"><a href="{{ route('cupboards.show', $cell->cupboard->slug) }}">{{ $cell->cupboard->title }}</a></li>
+    <li class="breadcrumb-item">{{ $cell->title }}</li>
+@endsection
+
+@section('content')
+    <h2 class="content-heading">Папки <small>{{ $cell->title }}</small></h2>
+    <div class="row">
+        @foreach($cell->folders as $folder)
+            <div class="col-sm-12 col-md-4 col-lg-3">
+                <div class="card text-center">
+                    <div class="d-flex justify-content-end align-items-center mt-1 mr-1">
+                        <a class="text-warning" data-toggle="tooltip" title="Редактировать" href="{{ route('folders.edit', $folder->slug) }}"><i class="fas fa-edit"></i></a>
+                        <form action="{{ route('folders.destroy', $folder->slug) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" onclick="return confirm('Вы уверены?')" class="btn text-danger" data-toggle="tooltip" title="Удалить"><i class="fas fa-trash"></i></button>
+                        </form>
+                    </div>
+                    <div class="card-body">
+                        <a href="{{ route('folders.show', $folder->slug) }}" class="card-title">{{ $folder->title }}</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
