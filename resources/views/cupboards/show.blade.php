44277425b3afff9eaf855230cc1e003024f5dba6
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-29 13:01:22 +0500

    Show cupboard cells in row

--- resources/views/cupboards/show.blade.php
+++ resources/views/cupboards/show.blade.php
@@ -9,21 +9,23 @@
 
 @section('content')
     <h2 class="content-heading">Ячейки <small>{{ $cupboard->title }}</small></h2>
-    @foreach($cupboard->cells as $cell)
-        <div class="col-sm-12 col-md-4 col-lg-3">
-            <div class="card text-center">
-                <div class="d-flex justify-content-end align-items-center mt-1 mr-1">
-                    <a class="text-warning" data-toggle="tooltip" title="Редактировать" href="{{ route('cupboards.edit', $cell->slug) }}"><i class="fas fa-edit"></i></a>
-                    <form action="{{ route('cupboards.destroy', $cell->slug) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" onclick="return confirm('Вы уверены?')" class="btn text-danger" data-toggle="tooltip" title="Удалить"><i class="fas fa-trash"></i></button>
-                    </form>
-                </div>
-                <div class="card-body">
-                    <a href="{{ route('cupboards.show', $cell->slug) }}" class="card-title">{{ $cell->title }}</a>
+    <div class="row">
+        @foreach($cupboard->cells as $cell)
+            <div class="col-sm-12 col-md-4 col-lg-3">
+                <div class="card text-center">
+                    <div class="d-flex justify-content-end align-items-center mt-1 mr-1">
+                        <a class="text-warning" data-toggle="tooltip" title="Редактировать" href="{{ route('cells.edit', $cell->slug) }}"><i class="fas fa-edit"></i></a>
+                        <form action="{{ route('cells.destroy', $cell->slug) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" onclick="return confirm('Вы уверены?')" class="btn text-danger" data-toggle="tooltip" title="Удалить"><i class="fas fa-trash"></i></button>
+                        </form>
+                    </div>
+                    <div class="card-body">
+                        <a href="{{ route('cells.show', $cell->slug) }}" class="card-title">{{ $cell->title }}</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 @endsection
