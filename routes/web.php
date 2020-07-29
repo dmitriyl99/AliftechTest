0ad4c35abecd7ff024f3f5630b3426e69aa94237
dmitriyl99 <dmitriyl1899@gmail.com>
2020-07-30 02:47:00 +0500

    Move api router to api.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('index');
 
 Route::resource('cupboards', 'CupboardController');
-Route::get('api/cupboards/{id}/cells', 'CupboardController@cells');
 Route::resource('cells', 'CellController');
 Route::resource('folders', 'FolderController');
 Route::resource('files', 'FileController');
